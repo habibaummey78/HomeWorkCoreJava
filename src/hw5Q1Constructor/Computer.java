9ce4998cc1394ba0bdaf3a8e6aa49482d3d4d8c1
habibaummey78 <habibaummey78@gmail.com>
2023-09-10 00:49:18 -0400

    update

--- src/hw5Q1Constructor/Computer.java
+++ src/hw5Q1Constructor/Computer.java
@@ -13,39 +13,31 @@ public class Computer {
 
 	// default Constructor declared
 
-	public Computer () {
-		System.out.println("This is from the default Constructor of Computer class");
+	public Computer() {
+		System.out.println("This is from the default Constructor of Computer class.");
 	}
-		// parameterized Constructor 01 declared	
-		
-		public Computer (String brand) { // here empName is called parameter, in line 5, it is variable
-			this.brand = brand;
-			System.out.println("My Computer Brand: " + brand);
-		}
-		
-		// parameterized Constructor 02 declared	
-		public Computer (String brand, String model, String operatingSystem, int price, char grade, boolean madeInUSA ) {
-			this.brand = brand;
-			this.model = model;
-			this.operatingSystem = operatingSystem;
-			this.price = price;
-			this.grade = grade;
-			this.madeInUSA = madeInUSA;
-			
-			
-			System.out.println("My Computer Brand: " + brand + ", Computer Model: " + model);
-		}
-		
-		// parameterized Constructor 02 declared	
-				public Computer (String computerBrandName, short computerModel, int computerPrice, char 
-						computerGrade, boolean madeInUSA) {
-					this.computerBrandName = computerBrandName;
-					this.computerModel = computerModel;
-					this.computerPrice = computerPrice;
-				//this.
-					System.out.println("My Computer Name: " + computerBrandName + ", Computer Brand Name: " + computerModel);
-				}
+	// parameterized Constructor 01 declared
 
+	public Computer(String brand) {
+		this.brand = brand;
 
+		System.out.println("My Computer Brand: " + brand);
+	}
+
+	// parameterized Constructor 02 declared
+
+	public Computer(String brand, String model, String operatingSystem, int price, char grade, boolean madeInUSA) {
+		this.brand = brand;
+		this.model = model;
+		this.operatingSystem = operatingSystem;
+		this.price = price;
+		this.grade = grade;
+		this.madeInUSA = madeInUSA;
+
+		System.out.println(
+				"My Computer Brand: " + brand + ", Computer Model: " + model + ", Operating System: " + operatingSystem
+						+ ", Price: " + price + ", Grade:" + grade + ", Is it made in USA? Ans: " + madeInUSA);
+
+	}
 
 }
--- src/hw5Q1Constructor/ComputerTest.java
+++ src/hw5Q1Constructor/ComputerTest.java
@@ -1,34 +1,18 @@
 package hw5Q1Constructor;
 
-
 public class ComputerTest {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		// default Constructor initialized
-				Computer computer = new Computer ();
 
-				//parameterized constructor 01 initialized
-				
-				Computer dell = new Computer("Dell Inspiron");
-				Computer hp= new Computer("Hp 1200");
+		Computer computer = new Computer();
 
-				// parameterized Constructor 02 Initialized
-				Computer asus = new Computer("Asus1200", 13330);
-				
-				
-				My Brand: Apple, Model: MacBook Air, Operating system: MacOS Mojave, 
-				Price: $800, Grade: A, and Made in the USA? Ans: false.
+		// parameterized constructor initialized
 
-						Step 7: I want the third outcome in the console for your windows 
-						Laptop configuration.
-				
-								
-			
-	
-		
-		
+		Computer mac = new Computer("Apple", "MacBook Air", "MacOS Mojave", 800, 'A', true);
+		Computer myComputer = new Computer("Dell", "Dell Inspiron", "DellOS cmv", 700, 'A', true);
+
 	}
 
 }
