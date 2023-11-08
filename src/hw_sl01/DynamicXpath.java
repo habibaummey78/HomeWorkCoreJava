90acbd89b20d761b1de74f2822ae00bc1a21255e
habibaummey78 <habibaummey78@gmail.com>
2023-11-08 02:32:26 -0500

    homework update

---
+++ src/hw_sl01/DynamicXpath.java
@@ -0,0 +1,18 @@
+package hw_sl01;
+
+public class DynamicXpath {
+
+	// a. Tag with a single attribute (from Samsung Website)
+	// input[@id='searchDomain']
+
+	// b. Tag with multiple attributes with 'and' logic
+	// input[@id='eppFlag' and @value='N']
+
+	// c. Tag with inner text
+	// label[text()='Enter your search keyword']
+
+	// d. Tag with partial inner text from any website.
+	// label[contains(text(), 'Email or')]
+
+	// above all had unique webelements.
+}
